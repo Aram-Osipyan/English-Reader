d86c7ec5bad30895db7435e4a00a8e56819ea085
Aram-Osipyan <osipyan.aram2017@ya.ru>
2022-10-13 15:49:10 +0300

    change translator service to dictionary.yandex

--- src/WordTranslator.js
+++ src/WordTranslator.js
@@ -1,10 +1,7 @@
 class WordTranslator{
-    _host = 'https://developers.lingvolive.com';
+    _host = 'https://dictionary.yandex.net';
     _apiKey = '<api key>';
-    _countryCodes = {
-        en:1033,
-        ru:1049
-    }
+
     /**
      *
      * @param word {string}
@@ -13,28 +10,22 @@ class WordTranslator{
      */
     async translate(word,srcLang = 'en', dstLang = 'ru'){
 
-        const tokenResponse = await fetch(`${this._host}/api/v1.1/authenticate`, {
-            method: 'POST',
-            headers: {
-                'Access-Control-Allow-Origin':'http://localhost:1234',
-                'Access-Control-Allow-Methods':'POST, GET, OPTIONS, DELETE, PUT',
-                'Access-Control-Allow-Headers': 'append,delete,entries,foreach,get,has,keys,set,values,Authorization',
-                'Access-Control-Allow-Credentials': true,
-                'Access-Control-Max-Age': 86400,
-                'Authorization': `Basic ${this._apiKey}`
-            }
+        const response = await fetch(this.generateTranslateApiURL(word, srcLang, dstLang), {
+            method: 'GET',
         });
 
-        const token = await tokenResponse.json();
-        const translationUrl = `${this._host}/api/v1/Translation?text=${word}&srcLang=${this._countryCodes[srcLang]}&dstLang=${this._countryCodes[dstLang]}`
-        const dictResponse = await fetch(translationUrl,{
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${token}`
-            }
-        })
+        return await response.json();
+    }
 
-        return await dictResponse.json();
+    /**
+     * @param word {string}
+     * @param srcLang {string}
+     * @param dstLang {string}
+     * @returns {string}
+     * @private
+     */
+    generateTranslateApiURL(word,srcLang = 'en', dstLang = 'ru'){
+        return `${this._host}/api/v1/dicservice.json/lookup?key=${this._apiKey}&lang=${srcLang}-${dstLang}&text=${word}`;
     }
 }
 
